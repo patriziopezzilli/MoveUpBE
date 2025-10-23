be3f48efd93c3d964e95b532a25f7a4edcf7266b
PATRIZIO.PEZZILLI <patrizio.pezzilli@unicredit.eu>
2025-10-23 11:02:38 +0200

    feat: add MongoDB configuration logging for debugging and enhance logging levels for MongoDB driver

--- src/main/java/com/moveup/MoveUpApplication.java
+++ src/main/java/com/moveup/MoveUpApplication.java
@@ -1,11 +1,15 @@
 package com.moveup;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import jakarta.annotation.PostConstruct;
 
 /**
  * MoveUp Backend Application
@@ -30,6 +34,17 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableAsync
 @EnableMethodSecurity(prePostEnabled = true)
 public class MoveUpApplication {
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void logMongoConfig() {
+        System.out.println("=== MONGO CONFIG DEBUG ===");
+        System.out.println("MONGODB_URI env var: " + env.getProperty("MONGODB_URI"));
+        System.out.println("spring.data.mongodb.uri: " + env.getProperty("spring.data.mongodb.uri"));
+        System.out.println("==========================");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MoveUpApplication.class, args);
